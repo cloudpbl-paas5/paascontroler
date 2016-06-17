75cdc0dad983c4037e877a7c120e9c72c06f1e89
caprice-j <skyfly0008@gmail.com>
2016-06-17 18:59:29 +0900

    add createRepositoryOf.rb. See Redmine Feature #660 for usage

---
+++ createRepositoryOf.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/ruby
+
+require 'open3' # see https://kaihar4.com/2015/01/18/ruby-shell.html
+
+paasUserName = ARGV[0]  # tomoya, yang, chen. Ruby does not store command name 'createRepositoryOf.rb' in ARGV
+paasRepoName = ARGV[1]
+
+unless paasRepoName
+  puts "usage:  ./#{$0} <userName> <repoName>"
+  exit 1
+end
+
+gitoliteAdminDirPath = '/home/gitadmin/gitolite-admin/'
+   targetTmpFilePath = gitoliteAdminDirPath + 'newRepo.tmp'
+gitoliteConfFilePath = gitoliteAdminDirPath + 'conf/gitolite.conf'
+
+# capture3 returns arrays of stdout, stderr, status
+
+# the 'cat' command does not handle stdin well, so I use a tmp file.
+
+File.write( targetTmpFilePath,
+"""
+repo    #{paasUserName}/#{paasRepoName}
+        RW+     =   gitadmin
+        RW      =   #{paasUserName}
+        R       =   gitreadonly
+""")
+
+
+out, err, status = Open3.capture3("cat #{targetTmpFilePath} >> #{gitoliteConfFilePath} ")
+
+puts "The following configuration is added in #{gitoliteConfFilePath} \n" + Open3.capture3("cat #{targetTmpFilePath}")[0] + "\n"
+
+Open3.capture3("rm #{targetTmpFilePath}")
+
+Open3.capture3("cd #{gitoliteAdminDirPath} && git commit -m 'add #{paasUserName}/#{paasRepoName} by #{$0}' #{gitoliteConfFilePath} && git push")
+
