60a0e730fd2eb256dd0fff40314be529f8803f98
tom-kita <t-kitazato@mcl.iis.u-tokyo.ac.jp>
2016-06-24 19:33:42 +0900

    insert user_repo, passwd

--- changeUserDB/changeUserDB.rb
+++ changeUserDB/changeUserDB.rb
@@ -3,19 +3,20 @@
 
 require 'mysql2'
 
-usrname = ARGV[0]
-ipaddr = ARGV[1]
-passwd = ARGV[2]
-repo_id = ARGV[3].to_i
-
-client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "cloud2016", :database => "gitRepo")
-
-# username, ipaddr, passwd, repo_id を引数にとり、ユーザー名からgitRipo.usr_repoを引く。
-# 該当するrowがなければ初プッシュなので、ユーザーDBを作成。gitRepo.UserDBにその情報を書く。
-results = client.query("SELECT * FROM gitRepo.Usr_repo WHERE usr_name = '#{usrname}'")
-if results.count == 0 then
-  client.query("CREATE USER '#{usrname}'@'#{ipaddr}' IDENTIFIED BY '#{passwd}'")
-  client.query("CREATE DATABASE #{usrname}")
-  client.query("GRANT ALL ON #{usrname}.* TO #{usrname}@#{ipaddr}")
-  client.query("INSERT INTO gitRepo.Usr_db (repo_id, db_name, usr_name, passwd) VALUES (#{repo_id}, '#{usrname}', '#{usrname}', '#{passwd}')")
+def changeUserDB(repo_name, user_name, ip_address)
+  client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "cloud2016", :database => "gitRepo")
+  # username, ipaddr, passwd, repo_id を引数にとり、ユーザー名からgitRipo.usr_repoを引く。
+  # 該当するrowがなければ初プッシュなので、ユーザーDBを作成。gitRepo.UserDBにその情報を書く。
+  client.query("INSERT INTO gitRepo.Usr_repo (usr_name, repo_name) VALUES ('#{user_name}', '#{repo_name}')")
+  results = client.query("SELECT id FROM gitRepo.Usr_repo WHERE usr_name = '#{user_name}'")
+  repo_id = 0
+  results.each do |row|
+    repo_id = row["id"]
+  end
+  passwd = [*0..9, *'a'..'z', *'A'..'Z'].sample(8).join
+  client.query("CREATE USER '#{user_name}'@'#{ip_address}' IDENTIFIED BY '#{passwd}'")
+  client.query("CREATE DATABASE #{user_name}")
+  client.query("GRANT ALL ON #{user_name}.* TO #{user_name}@#{ip_address}")
+  client.query("INSERT INTO gitRepo.Usr_db (repo_id, db_name, usr_name, passwd) VALUES (#{repo_id}, '#{user_name}', '#{user_name}', '#{passwd}')")
+  return passwd
 end
