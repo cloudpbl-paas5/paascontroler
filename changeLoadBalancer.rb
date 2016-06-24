dca1cd725a5052d289955b65989f8a62e968c038
s33333123 <s33333123@gmail.com>
2016-06-24 19:16:41 +0900

    www.paas -> www.paas2

--- changeLoadBalancer.rb
+++ changeLoadBalancer.rb
@@ -9,12 +9,12 @@ $reponame = ARGV[1]
 $IP = ARGV[2]
 $IPtodelete = ARGV[3]
 #add new lxc
-file = '/etc/nginx/sites-enabled/www.paas.exp.ci.i.u-tokyo.ac.jp'
+file = '/etc/nginx/sites-enabled/www.paas2.exp.ci.i.u-tokyo.ac.jp'
 if File.read(file).include?($IP) == false
     line = File.open(file,"a") do|line|
     line.puts "\n" + "upstream #$username-#$reponame {"
     line.puts "        server #$IP;"+"\n"+"}"+
-"server {"+"\n"+"        listen 80;"+"\n"+"        server_name #$username-#$reponame.www.paas.exp.ci.i.u-tokyo.ac.jp;"+"\n"+"        location / {"+"\n"+"                proxy_pass  http://#$username-#$reponame;"+"\n"+"                include /etc/nginx/proxy_params;"+"\n"+"        }"+"\n"+"}"
+"server {"+"\n"+"        listen 80;"+"\n"+"        server_name #$username-#$reponame.www.paas2.exp.ci.i.u-tokyo.ac.jp;"+"\n"+"        location / {"+"\n"+"                proxy_pass  http://#$username-#$reponame;"+"\n"+"                include /etc/nginx/proxy_params;"+"\n"+"        }"+"\n"+"}"
     end
 end    
 
